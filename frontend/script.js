b271a133a68d679cc192fa4eaf4f733e310760e7
Yoda <furifive5@gmail.com>
2026-05-07 13:49:09 +0300

    neshto

--- frontend/script.js
+++ frontend/script.js
@@ -4,10 +4,18 @@ const submit = document.getElementById("submitButton");
 
 submit.addEventListener("click", (e)=>{
     e.preventDefault();
-    fetch("", {
+    fetch("https://dice-game-m9fk.onrender.com/api/users/register", {
         method: "POST",
         body: JSON.stringify({username: userName.textContent, password: password.textContent})
     })
-    .then(()=>{window.alert("Succesfull registration")})
-    .catch((e)=>{window.alert(`Error: ${e}`)});
+    .then((res)=>{
+        console.log(res);
+        
+        if (res.status != 200) {
+            throw new Error(res.statusText);
+        }
+        console.log(res);
+        window.alert("Succesfull registration");
+    })
+    .catch((e)=>{window.alert(`${e}`)});
 });
