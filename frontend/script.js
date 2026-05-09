224aa9ac9906069a5aeb7f4f58aab754ea220390
Yoda <furifive5@gmail.com>
2026-05-09 14:14:22 +0300

    Neshto

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,53 @@
 const userName = document.getElementById("username-input");
 const password = document.getElementById("password-input");
-const submit = document.getElementById("submitButton");
+const registerButton = document.getElementById("registerButton");
+const loginButton = document.getElementById("loginButton");
 
-submit.addEventListener("click", (e)=>{
-    e.preventDefault();
+registerButton.addEventListener("click", (e)=>{
+    e.preventDefault;
+    register(userName.value, password.value)
+});
+loginButton.addEventListener("click", (e)=>{
+    e.preventDefault;
+    login(userName.value, password.value)
+});
+
+function register(username, password){
     fetch("https://dice-game-m9fk.onrender.com/api/users/register", {
         method: "POST",
-        body: JSON.stringify({username: userName.textContent, password: password.textContent})
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username: username, password: password})
     })
     .then((res)=>{
         console.log(res);
         
-        if (res.status != 200) {
+        if (res.status >= 300) {
             throw new Error(res.statusText);
         }
         console.log(res);
         window.alert("Succesfull registration");
     })
     .catch((e)=>{window.alert(`${e}`)});
-});
+}
+
+function login(username, password){
+    fetch("https://dice-game-m9fk.onrender.com/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username: username, password: password})
+    })
+    .then((res)=>{
+        console.log(res);
+        
+        if (res.status >= 300) {
+            throw new Error(res.statusText);
+        }
+        console.log(res);
+        window.alert("Succesfull login");
+    })
+    .catch((e)=>{window.alert(`${e}`)});
+}
